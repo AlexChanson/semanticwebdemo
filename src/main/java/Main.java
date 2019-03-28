ff4a63022d79c4ab3042fe4c3c6c63513c1e59fb
ben <bencrulis@gmail.com>
2019-03-28 20:30:13 +0100

    last tidying up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,6 +39,48 @@ public class Main {
 
     static PrefixMapping prefixMapping; // préfixes utilisés
 
+
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("\nChargement de l'instance");
+        loadFileJavaDemo();
+
+        System.out.println("\nChargement des ontologies");
+        block_enter();
+        loadFileDocumentManagerDemo();
+
+        System.out.println("\nCréation d'une classe et d'un individu");
+        block_enter();
+        addTriplets();
+
+        System.out.println("\nCréation du modèle d'inférence");
+        block_enter();
+        createInferenceModelDemo();
+
+
+        System.out.println("\nFabrication des préfixes");
+        block_enter();
+        setupPrefixesDemo();
+
+        System.out.println("\nRequêtage de l'instance");
+        sparqlQueryDemo();
+
+        // écrire l'instance
+        System.out.println("\nÉcriture de l'instance au format Turtle");
+        block_enter();
+        writeModelSemanticWebFormat(ressourceFolder + "inf.ttl", infered);
+
+        System.out.println("\nÉcriture de l'instance au format json-ld");
+        block_enter();
+        writeModelClassicFormat(ressourceFolder + "inf.json", infered);
+
+
+        System.out.println("\nÉcriture du nouveau schéma");
+        block_enter();
+        writeModelSemanticWebFormat(ressourceFolder+"nouveau_schema.ttl", fullSchema);
+
+    }
+
     public static void loadFileJavaDemo() throws FileNotFoundException {
 
         // Initialisation du modèle de graphe de l'instance
@@ -78,10 +120,16 @@ public class Main {
 
     	bibo = ontDocumentManager.getOntology("test2", modelSpec_bibo);
 
+    	System.out.println("\nChargement via le OntDocumentManager\n\n Avec la mauvaise URI");
+    	block_enter();
+
         System.out.println("\n");
     	System.out.println("Incorrectement chargé:");
     	System.out.println(bibo);
         System.out.println("\n");
+
+        System.out.println("\nChargement avec le bon URI");
+        block_enter();
 
         bibo = ontDocumentManager.getOntology("test", modelSpec_bibo);
         System.out.println("Correctement chargé:");
@@ -118,6 +166,7 @@ public class Main {
         prefixMapping.setNsPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
         prefixMapping.setNsPrefix("owl", "http://www.w3.org/2002/07/owl#");
         prefixMapping.setNsPrefix("event", "http://purl.org/NET/c4dm/event.owl#");
+        prefixMapping.setNsPrefix("dct", "http://purl.org/dc/terms/");
 
         prefixMapping.setNsPrefix("blt", "http://www.bl.uk/schemas/bibliographic/blterms#");
         prefixMapping.setNsPrefix("bibo", "http://purl.org/ontology/bibo/");
@@ -154,23 +203,24 @@ public class Main {
 
     public static void sparqlQueryDemo() {
 
-        //block_enter();
-    	//sparql_query("select distinct ?x ?y where {?x ?y event:Event}", data);
-
-    	//block_enter();
-        //sparql_query("select distinct ?x ?y where {?x ?y event:Event}", infered);
-
-        //block_enter();
-        //sparql_query("select distinct ?c where {?x rdf:type ?c}", infered);
-
+        System.out.println("\nRécupération des 100 premiers titres de livres");
         block_enter();
+        sparql_query("select distinct ?title where { ?x rdf:type bibo:Book . ?x dct:title ?title  } limit 100", infered);
+
+        System.out.println("\nRécupération des évènements sans inférences");
+        block_enter();
+    	sparql_query("select distinct ?x ?y where {?x ?y event:Event}", data);
+
+        System.out.println("\nRécupération des évènements avec inférences");
+        block_enter();
+        sparql_query("select distinct ?x ?y where {?x ?y event:Event}", infered);
 
         System.out.println("\nChercher les audio books");
-        //sparql_query("select ?x ?y ?z where { ?x rdf:type mo:AudioBook . ?x ?y ?z }", infered);
-
         block_enter();
+        sparql_query("select ?x ?y ?z where { ?x rdf:type mo:AudioBook . ?x ?y ?z }", infered);
 
         System.out.println("\nSuperclasses de l'audio book");
+        block_enter();
         sparql_query("select distinct ?x where { mo:audioBook1 rdf:type ?y . ?y rdfs:subClassOf ?x } ", infered);
 
     }
@@ -205,49 +255,6 @@ public class Main {
     }
 
 
-    public static void main(String[] args) throws Exception{
-
-        System.out.println("Chargement de l'instance");
-        loadFileJavaDemo();
-
-        System.out.println("Chargement des ontologies");
-        block_enter();
-        loadFileDocumentManagerDemo();
-
-        System.out.println("Création d'une classe et d'un individu");
-        block_enter();
-        addTriplets();
-
-        System.out.println("Création du modèle d'inférence");
-        block_enter();
-        createInferenceModelDemo();
-
-
-        System.out.println("Fabrication des préfixes");
-        block_enter();
-        setupPrefixesDemo();
-
-        System.out.println("Requêtage de l'instance");
-        sparqlQueryDemo();
-
-        // écrire l'instance
-        System.out.println("Écriture de l'instance au format Turtle");
-        block_enter();
-        writeModelSemanticWebFormat(ressourceFolder + "inf.ttl", infered);
-
-        System.out.println("Écriture de l'instance au format json-ld");
-        block_enter();
-        writeModelClassicFormat(ressourceFolder + "inf.json", infered);
-
-
-        System.out.println("Écriture du nouveau schéma");
-        block_enter();
-        writeModelSemanticWebFormat(ressourceFolder+"nouveau_schema.ttl", fullSchema);
-
-
-    }
-
-
     public static Iterable<QuerySolution> execSelect(QueryExecution queryExecution) {
         return new Iterable<QuerySolution>() {
             @Override
@@ -261,7 +268,7 @@ public class Main {
 
     public static void block_enter() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Appuyez sur entrée pour continuer...");
+        System.out.println("\nAppuyez sur entrée pour continuer...");
         sc.nextLine();
     }
 
